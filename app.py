4307b143a289130af5d9e06e6dbdd52dae71cce1
Phan Tan Dung <phantandung2003@gmail.com>
2025-02-19 12:57:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -64,4 +64,5 @@ def language_detection():
     return render_template('language_detection.html', language=language)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
+
